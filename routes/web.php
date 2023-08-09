7ad1e1f07fbc6ede151ce828ecc48957ce465e71
Karta Kusuma <kartakusuma1199@gmail.com>
2023-08-09 13:42:03 +0700

    Add user routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,12 @@ Route::get('logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/', [HomeController::class, 'dashboard'])->name('dashboard');
+    
+    Route::get('users', [UserController::class, 'index'])->name('users');
+    Route::get('users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('users/store', [UserController::class, 'store'])->name('users.store');
+    Route::get('users/{id}', [UserController::class, 'show'])->name('users.show');
+    Route::get('edit/{id}', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('update/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::post('destroy/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 });
