f07f0bf7b0ee564424c6d77dfbd7a36c9f0e6ec0
Karta Kusuma <kartakusuma1199@gmail.com>
2023-08-09 22:41:57 +0700

    Add edit distributor view

---
+++ resources/views/distributor/edit.blade.php
@@ -0,0 +1,84 @@
+@extends('main')
+
+@section('header')
+    <div class="content-header">
+        <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+            <h1 class="m-0">Distributors</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="{{ route('distributors') }}">Distributors</a></li>
+                <li class="breadcrumb-item active">Edit</li>
+            </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+@endsection
+
+@section('content')
+<section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <!-- general form elements -->
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title">Edit Distributor</h3>
+            </div>
+            <!-- /.card-header -->
+
+            <!-- form start -->
+            <form action="{{ route('distributors.update', $distributor->id) }}" method="post">
+              @csrf
+              @method('PATCH')
+              <div class="card-body">
+                <div class="form-group">
+                    <label for="exampleInputName1">Name</label>
+                    <input type="name" name="name" class="form-control" id="exampleInputName1" placeholder="Enter name" value="{{ $distributor->name }}">
+                    @error('name')
+                      <small>{{ $message }}</small>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email Address</label>
+                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" value="{{ $distributor->email }}">
+                    @error('email')
+                      <small>{{ $message }}</small>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputWhatsApp1">WhatsApp Number</label>
+                    <input type="whatsapp" name="whatsapp" class="form-control" id="exampleInputWhatsApp1" placeholder="Enter WhatsApp number" value="{{ $distributor->whatsapp }}">
+                    @error('whatsapp')
+                      <small>{{ $message }}</small>
+                    @enderror
+                </div>
+              </div>
+              <!-- /.card-body -->
+
+              <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+@endsection
+
+@section('scripts')
+    <!-- bs-custom-file-input -->
+    <script src="plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+    <script>
+        $(function () {
+            bsCustomFileInput.init();
+        });
+    </script>
+@endsection
