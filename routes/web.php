a16d7dd6e53f9530eff344ec30cb10647ba76cbc
Karta Kusuma <kartakusuma1199@gmail.com>
2023-08-09 22:12:50 +0700

    Add distributors routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DistributorController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
@@ -30,4 +31,11 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
     Route::patch('users/{id}/update', [UserController::class, 'update'])->name('users.update');
     Route::delete('users/{id}/destroy', [UserController::class, 'destroy'])->name('users.destroy');
+
+    Route::get('distributors', [DistributorController::class, 'index'])->name('distributors');
+    Route::get('distributors/create', [DistributorController::class, 'create'])->name('distributors.create');
+    Route::post('distributors/store', [DistributorController::class, 'store'])->name('distributors.store');
+    Route::get('distributors/{id}/edit', [DistributorController::class, 'edit'])->name('distributors.edit');
+    Route::patch('distributors/{id}/update', [DistributorController::class, 'update'])->name('distributors.update');
+    Route::delete('distributors/{id}/destroy', [DistributorController::class, 'destroy'])->name('distributors.destroy');
 });
