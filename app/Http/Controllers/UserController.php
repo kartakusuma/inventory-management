83d849e744545e0baae51180ef03de18600fd2a5
Karta Kusuma <kartakusuma1199@gmail.com>
2023-08-09 21:12:51 +0700

    Update user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,18 +42,47 @@ class UserController extends Controller
     }
 
     public function show($id) {
-        // 
+        $user = User::find($id);
+        return view('user.show', compact('user'));
     }
 
     public function edit($id) {
-        // 
+        $user = User::find($id);
+        return view('user.edit', compact('user'));
     }
 
     public function update(Request $request, $id) {
-        // 
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withInput()->withErrors($validator);
+        }
+
+        $data = $request->only(['name', 'email']);
+
+        if ($request->password) {
+            $validator = Validator::make($request->all(), [
+                'password' => 'min:8',
+            ]);
+
+            if ($validator->fails()) {
+                return redirect()->back()->withInput()->withErrors($validator);
+            }
+
+            $data['password'] = Hash::make($request->password);
+        }
+
+        User::whereId($id)->update($data);
+
+        return redirect()->route('users');
     }
 
     public function destroy($id) {
-        // 
+        User::whereId($id)->delete();
+
+        return redirect()->route('users');
     }
 }
