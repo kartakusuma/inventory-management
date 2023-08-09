a77de33f81b52a693678ecc5773686a828f622ee
Karta Kusuma <kartakusuma1199@gmail.com>
2023-08-09 22:11:10 +0700

    Create distributor index view

---
+++ resources/views/distributor/index.blade.php
@@ -0,0 +1,133 @@
+@extends('main')
+
+@section('links')
+  <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+@endsection
+
+@section('header')
+<div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+          <div class="col-sm-6">
+          <h1 class="m-0">Distributors</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+              {{-- <li class="breadcrumb-item"><a href="/">Home</a></li> --}}
+              <li class="breadcrumb-item active">Distributors</li>
+          </ol>
+          </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+@endsection
+
+@section('content')
+<section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+              <a href="{{ route('distributors.create') }}" class="btn btn-primary">Add New Distributor</a>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <table id="myTable" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($distributors as $distributor)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $distributor->name }}</td>
+                        <td>{{ $distributor->email }}</td>
+                        <td>
+                            <a href="{{ url('https://wa.me/'.$distributor->whatsapp) }}" target="blank" class="btn btn-success"><i class="fas fa-comment"></i> Chat</a>
+                            <a href="{{ route('users.edit', $distributor->id) }}" class="btn btn-warning"><i class="fas fa-pen"></i> Edit</a>
+                            <a data-toggle="modal" data-target="#modal-delete{{ $distributor->id }}" class="btn btn-danger"><i class="fas fa-trash"></i>Delete</a>
+                        </td>
+                    </tr>
+
+                    <div class="modal fade" id="modal-delete{{ $distributor->id }}">
+                        <div class="modal-dialog">
+                          <div class="modal-content bg-danger">
+                            <div class="modal-header">
+                              <h4 class="modal-title">Delete Distributor</h4>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                              <p>Are you sure to delete <b>{{ $distributor->name }}</b> data</p>
+                            </div>
+                            <div class="modal-footer justify-content-between">
+                              <button type="button" class="btn btn-outline-light" data-dismiss="modal">Close</button>
+                              <form action="{{ route('users.destroy', $distributor->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-outline-light">Yes</button></form>
+                            </div>
+                          </div>
+                          <!-- /.modal-content -->
+                        </div>
+                        <!-- /.modal-dialog -->
+                      </div>
+                      <!-- /.modal -->
+
+                @endforeach
+                </tbody>
+                {{-- <tfoot>
+                <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+                </tfoot> --}}
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+  </section>
+@endsection
+
+@section('scripts')
+    <!-- DataTables  & Plugins -->
+    <script src="plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="plugins/jszip/jszip.min.js"></script>
+    <script src="plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <!-- Page specific script -->
+    <script>
+        $(function () {
+            $("#myTable").DataTable({
+            "responsive": true, "lengthChange": false, "autoWidth": false,
+            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+            }).buttons().container().appendTo('#myTable_wrapper .col-md-6:eq(0)');
+        });
+    </script>
+@endsection
